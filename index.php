2d5efb0b554329e29a4ad1444c52085d1399434b
Darío Sande <dario.sande@gmail.com>
2013-10-15 16:15:14 -0200

    moved /env.php code back to index.php

--- index.php
+++ index.php
@@ -19,7 +19,18 @@
  *
  */
 
-require_once "env.php";
+require_once "custom/env.php";
+$_host_name = $_SERVER[ 'SERVER_NAME' ];
+$_this_env = 'production'; // this is the default env
+foreach ( $_environments_list as $env_name => $env_urls ) {
+	foreach ( $env_urls as $url ) {
+		if ( preg_match( "/{$url}$/", $_host_name ) ) {
+			$_this_env = $env_name; // boom, we found it
+			break 2;
+		}
+	}
+}
+define( 'ENVIRONMENT', $_this_env );
 
 /*
  *---------------------------------------------------------------
