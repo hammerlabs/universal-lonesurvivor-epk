d8a09c2ebe82aa410377ffae3a64e2b79e23202c
Sergio Tashdjian <gtashi@gmail.com>
2013-10-16 19:20:47 -0200

    Add Tweenlite to libs

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -32,7 +32,7 @@ class Main extends HLabs_Controller {
 
 		//Javascript added at the end of the document
         $this->_add_js("js/libs/requestAnimationFrame.js","footer",array("weight"=>-200));
-        $this->_add_js("http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js","footer");
+        $this->_add_js("js/libs/TweenLite.min.js","footer");
         $this->_add_js("js/libs/jquery.touchSwipe.js","footer");
         $this->_add_js("js/libs/jquery.parallax.js","footer");
 		$this->_add_js("js/libs/binaryajax.js","footer");
