aa264de7cae370f01f8a006964f9e7031e52ba89
Kevin Hammer <kevin@hammerlabs.com>
2013-10-18 08:24:38 -0700

    adding lone survivor meta data and startup scripts

--- application/config/my_base.php
+++ application/config/my_base.php
@@ -18,11 +18,11 @@ $config['check_folders']=array(
 );
 
 //Meta tags, FB tags, etc.
-$config["title"] = "Site Title"; 
-$config["desc"] = "Site Description"; 
-$config["url"] = "URL to production site"; 
-$config["image"] = "URL to image at root of site to be used for social postings"; 
+$config["title"] = "Lone Survivor | Official Movie Site | Universal Pictures"; 
+$config["desc"] = "Based on the failed June 28, 2005 mission, Operation Red Wings. Four members of SEAL Team 10, were tasked with the mission to capture or kill notorious Taliban leader, Ahmad Shahd. Marcus Luttrell was the only member of his team to survive."; 
+$config["url"] = "http://www.lonesurvivorfilm.com"; 
+$config["image"] = "http://www.lonesurvivorfilm.com/lone_survivor.jpg"; 
 $config["facebook_url"] = "URL to facebook landing page";    
-$config["keywords"] = "Site Keywords";    
-$config["og_title"] = "Site title to display in facebook";  
+$config["keywords"] = "Lone Survivor Mark Wahlberg Taylor Kitsch Alexander Ludwig Eric Bana Ben Foster Emile Hirsch";    
+$config["og_title"] = "Follow The Latest News From The Lone Survivor";  
 $config["ga_account"] = ""; //Google Analytics account. Leave empty to disable.
--- assets/js/main.js
+++ assets/js/main.js
@@ -64,18 +64,18 @@
 	    }});
 
 		// jQuery compatible initialization of Facebook JS SDK
-		/*$.ajaxSetup({ cache: true });
+		$.ajaxSetup({ cache: true });
 		$.getScript('//connect.facebook.net/en_US/all.js', function(){
 			// put all FB JS SDK initialization logic in here so that we ensure the FB API is available first
 			FB.init({
-				appId      : '000000000000000',                    // App ID from the app dashboard
+				appId      : '167842429947070',                    // App ID from the app dashboard
 				channelUrl : '//'+location.host+'/channel.html',   // Channel file for x-domain comms
 				status     : true,                                 // Check Facebook Login status
 				xfbml      : true                                  // Look for social plugins on the page
 			});
 			//$('#loginbutton,#feedbutton').removeAttr('disabled');
 	    	//FB.getLoginStatus(updateStatusCallback);
-		});*/
+		});
 
 
 	});
@@ -103,7 +103,75 @@
 	}
 
 	function siteReady(){
-		console.log("siteReady", "now add click listeners and other initialization code to the UI, start build in animations, etc");
+	/* ==========================================================================
+	   add movie ticket look up feature
+	   ========================================================================== */
+		var zipSelector = "#zip";
+		var fandangoSelector = "a.fandango";
+		var movieticketsSelector = "a.movietickets";
+		// setup tickets box
+		$( zipSelector ).keyup(function () { 
+		    if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+		       this.value = this.value.replace(/[^0-9\.]/g, '');
+		    }
+		    if (this.value.length > 5) {
+				this.value = this.value.substr(0, 5);
+		    }
+		});
+		$( fandangoSelector ).click(function(e) {
+			if ($( zipSelector ).val().length == 5) {
+				e.preventDefault();
+				window.open(this.href + "?location=" + $( zipSelector ).val(), "_blank")
+			}
+		})
+		$( movieticketsSelector ).click(function(e) {
+			if ($( zipSelector ).val().length == 5) {
+				e.preventDefault();
+				window.open(this.href + "&SearchZip=" + $( zipSelector ).val(), "_blank")
+			}
+		})
+
+
+	/* ==========================================================================
+	   add ambient music feature
+	   ========================================================================== */
+		var audioUrl = "/audio/ambience.mp3";
+		var audioSelector = ".audio-control";
+		//Add ambience sound
+		window.amb_snd = new Audio( audioUrl ); // buffers automatically when created
+		amb_snd.addEventListener('ended', function() {
+		    this.currentTime = 0;
+		    this.play();
+		}, false);			
+		amb_snd.play();
+
+		//Add sound control behavior
+		$( audioSelector ).addClass("noSwipe").click(function(){
+			if ($(this).hasClass("off")){
+				$(this).removeClass("off");
+				amb_snd.play();
+			}
+			else{
+				$(this).addClass("off");
+				amb_snd.pause();
+			}
+		});			
+
+		$(window).focus(function() {
+			if (! $( audioSelector ).hasClass("off")){
+				amb_snd.play();
+			}
+	    });
+
+	    $(window).blur(function() {
+	    	if (! $( audioSelector ).hasClass("off")){
+		 		amb_snd.pause();		        
+		 		//In iPad (and probably needed on other tablets), mark the audio as mutted cos user will need to tap to turn it back on
+		 		if (window.isiPad){
+		 			$( audioSelector ).addClass("off");
+		 		}
+		 	}
+	    });
 	}
 
 	
