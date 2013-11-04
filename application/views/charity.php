3a733bec8f84c7549cc41780ff62b41afd530419
Kevin Hammer <kevin@hammerlabs.com>
2013-11-04 13:09:35 -0800

    new charity page layout

--- application/views/charity.php
+++ application/views/charity.php
@@ -11,21 +11,20 @@
 
             <div class="main-container hide">
     	        <div id="charity">
-                    <div class="title2">SUPPORT OUR HEROES</div>
-                    <div class="charity-wrapper lone_survivor_foundation">
+                    <div class="title2">LONE SURVIVOR FUND</div>
+                    <div class="content one">The Lone Survivor Fund will distribute all donations made in honor of the film to three leading veteran<br/>and military focused charities: Got Your 6, the Navy Seal Foundation and the Lone Survivor Foundation.</div>
+                    <div class="donate-wrapper">
                         <a href="http://lonesurvivorfoundation.org/donate/" target="_blank">DONATE</a>
-                        <span class="title1 blocktext">LONE SURVIVOR FOUNDATION</span>
-                        <div>The Lone Survivor Foundation restores, empowers, and renews hope for our wounded warriors and their families through health, wellness, and therapeutic support.</div>
+                    </div>
+                    <div class="charity-wrapper lone_survivor_foundation">
+                        <div>The Lone Survivor Foundation restores, empowers, and renews hope for our wounded service members and their families through health, wellness, and therapeutic support.  The vision of the Lone Survivor Foundation is to provide exceptional therapeutic, outdoor, and unique opportunities that optimize recovery and healing of affected American wounded service members and their families.  The Lone Survivor Foundation has invested [$] to help [X] of veterans and their families.</div>
                     </div>
                     <div class="charity-wrapper navy_seal_foundation">
-                        <a href="http://www.navysealfoundation.org/donations/" target="_blank">DONATE</a>
-                        <span class="title1 blocktext">NAVY SEAL FOUNDATION</span>
-                        <div>The Navy SEAL Foundation coordinates closely with Naval Special Warfare commands to support critical needs of active-duty operators while also providing resources for NSW veterans.</div>
+                        <div>The Navy SEAL Foundation was established to support U.S. Navy SEALs, Special Warfare Combatant-craft Crewman, Naval Special Warfare (NSW) support personnel, and their families. The Foundation’s work is focused on five pillars: warrior support and family services, educational opportunities, tragedy assistance, survivor support, and legacy activities. Through the generosity of donors, the Navy SEAL Foundation provided more than $4.2 million in 2012 to help set the conditions that enable NSW operators and their family members to not only be successful, but to achieve their fullest potential. Among other vital areas of support, the Foundation provided scholarships and tuition assistance for more than 1,000 NSW personnel and their dependents; more than $500,000 to help cover critical costs during times of tragedy for families; and over $409,000 for child development programs.</div>
                     </div>
                     <div class="charity-wrapper got_your_6">
-                        <a href="https://www.gotyour6.org/donate/" target="_blank">DONATE</a>
-                        <span class="title1 blocktext">GOT YOUR 6</span>
-                        <div>Got Your 6 aims to bridge the civilian-military divide by creating a new conversation in America, so that veterans and military families are perceived as leaders and civic assets.</div>
+                        <div>Got Your 6 is a collective impact campaign designed to help bridge the military-civilian divide. Combining the reach and resources of the entertainment industry with the expertise and commitment of more than 30 non-profit organizations, the campaign works to advance the conversation in America so veterans and military families are seen as leaders and civic assets who will invigorate our communities. In 2013, Got Your 6 distributed grants totaling nearly $3 million to a dozen veteran-focused non-profit partners.</div>
                     </div>
+                    <div class="content two">The Lone Survivor Fund was formed as a philanthropic companion to the major motion picture Lone Survivor. The Lone Survivor Fund is maintained by Be the Change, Inc., a public charity recognized by the IRS as tax-exempt under section 501(c)(3) of the Internal Revenue Code.  Contributions to the Fund are tax-deductible to the full extent allowed by law and will go to support the efforts of threeorganizations making a difference in the lives of veterans and military families: Got Your 6, the Lone Survivor Foundation, and the Navy Seal Foundation.</div>
                 </div>
     	    </div>
